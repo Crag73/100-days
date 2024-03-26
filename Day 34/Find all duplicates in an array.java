f02eb25b48677433eae20a4f664a3aa946346205
Divyam <divyambajal73@gmail.com>
2024-03-26 22:13:09 +0530

    Day 34

---
+++ Day 34/Find all duplicates in an array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans=new ArrayList<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int index=Math.abs(nums[i])-1;
+            if(nums[index]<0){
+                ans.add(Math.abs(nums[i]));
+            }
+            nums[index]=Math.abs(nums[index])*-1;
+        }
+        return ans;
+    }
+}
---
+++ Day 34/Find all numbers that disappeared.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> ans=new ArrayList<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            int index=Math.abs(nums[i])-1;
+            nums[index]=Math.abs(nums[index])*-1;
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                ans.add(i+1);
+            }
+        }
+        return ans;
+    }
+}
