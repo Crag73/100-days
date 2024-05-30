d54a13d1ea009f8b890c95aec8505bdd2d513c6c
Divyam <divyambajal73@gmail.com>
2024-05-30 10:10:49 +0530

    Day 75

---
+++ Day 74/Boats to Save People.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numRescueBoats(int[] arr, int limit) {
+        int ans=0;
+        Arrays.sort(arr);
+        int left=0;
+        int right=arr.length-1;
+        while(left<=right){
+            if(arr[left]+arr[right]<=limit){
+                left++;
+            }
+            right--;
+            ans++;
+        }
+        return ans;
+    }
+}
